4035614d4449bc75d2c1e88f6cf4d2a3866eca6c
Mateus <mtsmlima@gmail.com>
2025-03-10 19:38:09 -0300

    Added new tags

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,20 +6,52 @@ import moon from '../../assets/images/moon.svg'
 import './headerStyle.scss'
 
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+   darkMode: boolean
+   handleTheme: () => void
+}
+
+
+export const Header: React.FC<HeaderProps> = (props) => {
    return (
       <header className="header">
          <div className="header__wrapper">
             <Link to="/" className="header__movies">
                <h1 className="header__title">Movies</h1>
 
-               <img src={camera} width={30} alt="Image of a camera" />
+               <img
+               className={props.darkMode ? "header__title-img" : ""}
+               src={camera}
+               width={30}
+               alt="Image of a camera"
+               />
             </Link>
 
-            <button type='button' className="header__theme-btn">
-               Dark mode
-               {/* <img src={sun} alt="Icon of the sun" /> */}
-               <img src={moon} alt="Icon of the moon" />
+            <button
+               type='button' className="header__theme-btn"
+               onClick={props.handleTheme}
+            >
+               {props.darkMode ?
+                  <>
+                     Light mode
+                     < img
+                        className='header__sun-img'
+                        width={20}
+                        src={sun}
+                        alt="Icon of the sun"
+                     />
+                  </>
+                  :
+                  <>
+                     Dark Mode
+                     <img
+                        className='header__moon-img'
+                        width={20}
+                        src={moon}
+                        alt="Icon of the moon"
+                     />
+                  </>
+               }
             </button>
          </div>
       </header>

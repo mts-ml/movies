05804799ec871f8bc4dfbebfb7eba4e340d957e2
Mateus <mtsmlima@gmail.com>
2025-03-10 19:38:23 -0300

    Added new tags

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,36 @@
+import { useOutletContext, Link } from 'react-router-dom'
+import { Movies } from '../Layout/Layout'
+
+import './homeStyle.scss'
+
+
 export const Home: React.FC = () => {
+   const moviesArray: Movies[] = useOutletContext()
+
+   const movies = moviesArray.map(movie => (
+      <Link
+         to={`movie/${movie.id}`}
+         key={movie.id}
+         className="movie"
+      >
+         <div className="movie__content">
+            <img className='movie__img' src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Poster of the movie ${movie.title}`} />
+
+            <div className="movie__details">
+               <h3 className="movie__title">{movie.title}</h3>
+
+               <p className="movie__rating">⭐ {movie.vote_average.toFixed(1)}</p>
+               
+               <p className="movie__release">📅 {movie.release_date}</p>
+            </div>
+         </div>
+      </Link>
+
+   ))
+
    return (
-      <h1>HOME</h1>
+      <section className="home__movies">
+         {movies}
+      </section>
    )
 }

cd980442cefb218d2bcd529baf203cb988129601
Mateus <mtsmlima@gmail.com>
2025-03-11 14:03:39 -0300

    Added new interface and state

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,48 @@
 import { useOutletContext, Link } from 'react-router-dom'
-import { Movies } from '../Layout/Layout'
+import { Genres, Movies } from '../Layout/Layout'
+import { ThreeDots } from 'react-loader-spinner';
 
 import './homeStyle.scss'
 
 
-export const Home: React.FC = () => {
-   const moviesArray: Movies[] = useOutletContext()
+export interface OutletContextType {
+   movies: Movies[];
+   genres: Genres[];
+}
 
-   const movies = moviesArray.map(movie => (
+
+export function formatDate(dateString: string, locale = navigator.language) {
+   const date = new Date(dateString)
+   return new Intl.DateTimeFormat(locale, {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+   })
+      .format(date)
+      .replace(/-/g, "/")
+}
+
+
+export const Home: React.FC = () => {
+   const { movies = [], genres = [] } = useOutletContext<OutletContextType>()
+
+   if (movies.length <= 0 || genres.length <= 0) {
+      return (
+         <div id='loading'>
+            <span>Loading...</span>
+            <ThreeDots
+               visible={true}
+               height={80}
+               width={80}
+               color="rgb(80, 135, 167)"
+               radius="9"
+               ariaLabel="three-dots-loading"
+            />
+         </div>
+      )
+   }
+
+   const moviesList = movies.map(movie => (
       <Link
          to={`movie/${movie.id}`}
          key={movie.id}
@@ -20,17 +55,17 @@ export const Home: React.FC = () => {
                <h3 className="movie__title">{movie.title}</h3>
 
                <p className="movie__rating">⭐ {movie.vote_average.toFixed(1)}</p>
-               
-               <p className="movie__release">📅 {movie.release_date}</p>
+
+               <p className="movie__release">📅 {formatDate(movie.release_date)}</p>
             </div>
          </div>
       </Link>
-
    ))
+
 
    return (
       <section className="home__movies">
-         {movies}
+         {moviesList}
       </section>
    )
 }

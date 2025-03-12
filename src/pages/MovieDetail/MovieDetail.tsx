b5d193fe56f001b451143513429046fd33d27e9e
Mateus <mtsmlima@gmail.com>
2025-03-12 19:12:02 -0300

    Added new fetch and logic to show on UI

--- src/pages/MovieDetail/MovieDetail.tsx
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -2,15 +2,45 @@ import { Movies } from "../Layout/Layout";
 import { formatDate, OutletContextType } from "../Home/Home";
 import { useOutletContext, useParams, Link, useLocation } from "react-router-dom"
 import { ThreeDots } from 'react-loader-spinner';
+import { useEffect, useState } from "react";
 import leftArrow from '../../assets/images/left-arrow.png'
+import castImg from '../../assets/images/person.svg'
 
 import "./movieDetailStyle.scss"
+
+
+interface MovieDetailProps {
+   character: string
+   id: number
+   name: string
+   popularity: number
+   profile_path: string
+}
 
 
 export const MovieDetail: React.FC = () => {
    const { movies } = useOutletContext<OutletContextType>();
 
+   const [cast, setCast] = useState<MovieDetailProps[]>([])
+
    const { id } = useParams()
+
+   const API_KEY: string = import.meta.env.VITE_OMDB_API_KEY
+
+   useEffect(() => {
+      async function fetchData() {
+         try {
+            const [castResponse] = await Promise.all([
+               fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}`)
+            ])
+            const castData = await castResponse.json()
+            setCast(castData.cast)
+         } catch (error) {
+            console.log(`Error fetching data: ${error}`)
+         }
+      }
+      fetchData()
+   }, [id])
 
    const pageLocation = useLocation()
 
@@ -50,27 +80,45 @@ export const MovieDetail: React.FC = () => {
       );
    }
 
-   console.log(movie)
-
-
+   console.log(cast)
    return (
-      <section className="movie-detail">
-         <Link className="movie-detail-link" to={`..?${movieGenreState}`}>
-            <img src={leftArrow} alt="Icon of a left arrow" />
-            Back to {movieGenreType} movies
-         </Link>
+      <>
+         <section className="movie-detail">
+            <Link className="movie-detail-link" to={`..?${movieGenreState}`}>
+               <img src={leftArrow} alt="Icon of a left arrow" />
+               Back to {movieGenreType} movies
+            </Link>
 
-         <img className='movie-detail__img' src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Poster of the movie ${movie.title}`} />
+            <img className='movie-detail__img' src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Poster of the movie ${movie.title}`} />
 
-         <h2 className="movie-detail__title"><span>Title:</span> {movie.title}</h2>
+            <h2 className="movie-detail__title"><span>Title:</span> {movie.title}</h2>
 
-         <p className="movie-detail__rating">⭐ {movie.vote_average.toFixed(1)}</p>
+            <p className="movie-detail__rating">⭐ {movie.vote_average.toFixed(1)}</p>
 
-         <p className="movie-detail__release-date">📅 {formatDate(movie.release_date)}</p>
+            <p className="movie-detail__release-date">📅 {formatDate(movie.release_date)}</p>
 
-         <p className="movie-detail__description">
-            Sinopse: {movie.overview}
-         </p>
-      </section>
+            <p className="movie-detail__description">
+               Sinopse: {movie.overview}
+            </p>
+         </section>
+
+         <h3 className="movie-detail__cast-name">Cast</h3>
+
+         <section className="movie-detail__cast">
+            {cast.slice(0, 6).map(actor => {
+               const castImage = actor.profile_path ? `https://image.tmdb.org/t/p/w500/${actor.profile_path}` : castImg
+
+               return <div key={actor.id} className="movie-detail__div">
+                  <img src={castImage} alt={`Picture of ${actor.name}`} />
+
+                  <h4 className="movie-detail__cast_name">{actor.name}</h4>
+
+                  <p className="movie-detail__popularity">
+                     Popularity: {actor.popularity.toFixed(2)}
+                  </p>
+               </div>
+})}
+         </section>
+      </>
    )
 }

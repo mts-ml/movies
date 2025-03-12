9c1341cd64336f6ee1707b871d077ad6b1220db0
Mateus <mtsmlima@gmail.com>
2025-03-12 14:32:31 -0300

    Added logic to load movies when reaches the end of screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,25 @@
 import { useOutletContext, Link, useSearchParams } from 'react-router-dom'
 import { Genres, Movies } from '../Layout/Layout'
 import { ThreeDots } from 'react-loader-spinner';
+import { useEffect } from 'react';
 
 import './homeStyle.scss'
-import { useEffect, useState } from 'react';
 
 
 export interface OutletContextType {
-   movies: Movies[];
-   genres: Genres[];
+   movies: Movies[]
+   genres: Genres[]
    page: number
    setPage: React.Dispatch<React.SetStateAction<number>>
    totalPages: number
+   isLoading: boolean
+   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export function formatDate(dateString: string, locale = navigator.language) {
    const date = new Date(dateString)
+   if (isNaN(date.getTime())) return "Invalid Date"
+
    return new Intl.DateTimeFormat(locale, {
       day: "2-digit",
       month: "2-digit",
@@ -25,10 +29,31 @@ export function formatDate(dateString: string, locale = navigator.language) {
       .replace(/-/g, "/")
 }
 
-
-
 export const Home: React.FC = () => {
-   const { movies = [], genres = [], page, setPage, totalPages } = useOutletContext<OutletContextType>()
+   const { movies, genres, page, setPage, totalPages, isLoading, setIsLoading } = useOutletContext<OutletContextType>()
+
+   useEffect(() => {
+      const handleScroll = () => {
+         if (isLoading || page >= totalPages) return
+
+         const scrollDistanceFromTop = document.documentElement.scrollTop;
+         const totalPageHeight = document.documentElement.scrollHeight;
+         const visibleViewportHeight = document.documentElement.clientHeight;
+
+
+         if (scrollDistanceFromTop + visibleViewportHeight >= totalPageHeight - 100) {
+            setIsLoading(true)
+            setPage(previousState => previousState + 1)
+         }
+      }
+
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+   }, [isLoading, page, totalPages, setPage])
+
+   useEffect(() => {
+      setIsLoading(false)
+   }, [movies])
 
    const [searchParams, setSearchParams] = useSearchParams();
 
@@ -36,35 +61,7 @@ export const Home: React.FC = () => {
 
    const filteredMovies = getMoviesByGenre ? movies.filter(movie => movie.genre_ids.includes(Number(getMoviesByGenre))) : movies
 
-   const hasMoreMovies = filteredMovies.length > 0 && page < totalPages
-
-   function loadMoreMovies() {
-      if (hasMoreMovies) {
-         setPage(previousState => previousState + 1)
-      }
-   }
-
-   useEffect(() => {
-      function handleScroll() {
-         const { scrollTop, clientHeight, scrollHeight } = document.documentElement
-
-         // Verifies if user is on the end of page
-         if (scrollTop + clientHeight >= scrollHeight - 100 && hasMoreMovies) {
-            loadMoreMovies()
-         }
-      }
-
-      // Adiciona o evento de rolagem
-      window.addEventListener("scroll", handleScroll)
-
-      // Remove o evento ao desmontar o componente
-      return () => {
-         window.removeEventListener("scroll", handleScroll);
-      }
-   }, [page, totalPages, filteredMovies.length])
-
-
-   if (movies.length === 0 || genres.length === 0) {
+   if (!movies?.length || !genres?.length) {
       return (
          <div id='loading' >
             <span>Loading...</span>
@@ -80,6 +77,8 @@ export const Home: React.FC = () => {
       )
    }
 
+   const genreName = genres.find(genre => genre.id === Number(getMoviesByGenre))?.name || "all"
+
    const uniqueIdsSet = new Set<number>()
    const moviesList = filteredMovies.filter(movie => {
       if (!uniqueIdsSet.has(movie.id)) {
@@ -90,6 +89,7 @@ export const Home: React.FC = () => {
    }).map(movie => (
       <Link
          to={`movie/${movie.id}`}
+         state={{ searchUrl: searchParams.toString(), type: genreName }}
          key={movie.id}
          className="movie"
       >
@@ -113,27 +113,44 @@ export const Home: React.FC = () => {
          <ul className="movie__genres">
             {genres.map(genre => (
                <li key={genre.id}>
-                  <Link className='movie__link' to={`?genre=${genre.id}`}>{genre.name}</Link>
+                  <Link
+                     className={`movie__link ${getMoviesByGenre === String(genre.id) ? "active" : ""}`}
+                     to={`?genre=${genre.id}`}>
+                     {genre.name}
+                  </Link>
                </li>
             ))}
          </ul>
 
+         <button
+            type='button'
+            className={`movie__clear-filter ${getMoviesByGenre ? "movie__btn-visible" : ""}`}
+            onClick={() => setSearchParams({})}
+         >Clear filter</button>
+
          <section className="home__movies">
             {moviesList}
-
-            {hasMoreMovies && (
-               <div className="loading-more">
-                  <ThreeDots
-                     visible={true}
-                     height={40}
-                     width={40}
-                     color="rgb(80, 135, 167)"
-                     radius="9"
-                     ariaLabel="three-dots-loading"
-                  />
-               </div>
-            )}
          </section>
+
+         {isLoading && (
+            <div className="loading-more">
+               <ThreeDots
+                  visible={true}
+                  height={80}
+                  width={80}
+                  color="rgb(80, 135, 167)"
+                  radius="9"
+               />
+            </div>
+         )}
+
+         {!isLoading && page < totalPages && (
+            <button type='button'
+               className="load-more-button"
+               onClick={() => setPage(previousState => previousState + 1)}
+            >Page {page} - Click for next
+            </button>
+         )}
       </main>
    )
 }

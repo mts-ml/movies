99e6e53d1297f1d0765a40b5172dbcb32bd918e4
Mateus <mtsmlima@gmail.com>
2025-03-17 16:36:48 -0300

    Added loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Home } from './pages/Home/Home'
 import { MovieDetail } from './pages/MovieDetail/MovieDetail'
 import ErrorPage from './pages/ErrorPage/ErrorPage'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { homeLoader, movieDetailLoader } from './moviesLoader'
 
 import './styles/globalStyles.scss'
 
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route element={<Layout />} errorElement={<ErrorPage />}>
-    <Route path="/" element={<Home />} />
-    <Route path="movie/:id" element={<MovieDetail />} />
+  <Route element={<Layout />} errorElement={<ErrorPage />} >
+    <Route path="/" element={<Home />} loader={homeLoader} />
+    <Route path="movie/:id" element={<MovieDetail />} loader={movieDetailLoader} />
 
   </Route>
 ))
